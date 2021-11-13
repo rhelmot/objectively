761aba9ea56d2c7b592e48f242ddd4533462f78d
Audrey Dutcher <audrey@rhelmot.io>
2021-11-13 03:52:21 -0700

    Compiler can be used as a library

--- py_compiler/compiler.py
+++ py_compiler/compiler.py
@@ -7,7 +7,12 @@ import pprint
 import lexer as lexer_module
 import parser as parser_module
 
-lexer = lex(module=lexer_module)
-parser = yacc(module=parser_module)
-result = parser.parse(open(sys.argv[1]).read(), lexer=lexer, debug='--debug' in sys.argv)
-open(sys.argv[2], 'wb').write(result.link())
+def oly_compile(in_text, debug=False):
+    lexer = lex(module=lexer_module)
+    parser = yacc(module=parser_module)
+    result = parser.parse(in_text, lexer=lexer, debug=debug)
+    return result.link()
+
+if __name__ == '__main__':
+    result = oly_compile(open(sys.argv[1]).read(), debug='--debug' in sys.argv)
+    open(sys.argv[2], 'wb').write(result)
